ec76a0b6e97b777c10e5d9210b6a9d5a1e677311
lightebube <lightebube751@gmail.com>
2023-06-15 16:08:46 -0700

    Hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * main-Prints the message "with proper grammar, but the outcome is a piece of art,"
+ * Return - Always 0 (Success)
+ */
+
+int main(void)
+{
+	const char *message = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", message);
+
+	return (0);
+}
