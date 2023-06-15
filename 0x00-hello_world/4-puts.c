634ba0666f350781826260bc39b263b6d1736840
lightebube <lightebube751@gmail.com>
2023-06-15 15:29:35 -0700

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
-/*main-Entry point
- *
- * Return = Always 0 (Success)
+/*main-Prints message "\"Programming is like building a multilingual puzzle"
+ *Return = Always 0 (Success)
  */
 
 int main(void) 
 {
 	const char *message = "\"Programming is like building a multilingual puzzle";
+
 	puts(message);
 
 	return (0);
