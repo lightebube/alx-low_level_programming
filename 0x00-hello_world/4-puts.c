7806cee485aa4402ff0b12c20c06883db386db56
lightebube <lightebube751@gmail.com>
2023-06-15 15:51:46 -0700

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- *main-Prints message "\"Programming is like building a multilingual puzzle"
+ *main(void)-Prints message "\"Programming is like building a multilingual puzzle"
  *Return = Always 0 (Success)
  */
 
