cf28b6c5a3f9369215208c8d14fcc87d9948d842
lightebube <lightebube751@gmail.com>
2023-06-19 14:38:46 -0700

    Hello worlds

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
-/*
+
+/**
  * Main = Prints the message "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
  * Return = Always 1 
  */
-
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	long unsigned int message = and that piece of art is useful"\ - Dora Korpar, 2015-10-19;
 
 	write(2, "\n", message);
 
