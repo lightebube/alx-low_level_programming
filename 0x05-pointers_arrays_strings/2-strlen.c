05bc0b866e69c87d799491218483f8b260685f79
lightebube <lightebube751@gmail.com>
2023-06-26 09:47:49 -0700

    Pointers and arrays

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Prints length of a string
+ * @s: string
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	*s = s;
+
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string
+ * @str: string
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
