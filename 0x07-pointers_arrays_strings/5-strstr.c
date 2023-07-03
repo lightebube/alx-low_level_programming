73089a2b9f573d8903971f8269f2f13b4cc434e0
lightebube <lightebube751@gmail.com>
2023-07-03 15:50:09 -0700

    A function that locates a substring.

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strstr - Locates a substring in a string
@@ -9,15 +10,5 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
-
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		if (haystack[i] == needle[i])
-		{
-			return (needle);
-		}
-	}
-
-	return (NULL);
+	return (strstr(haystack, needle));
 }
