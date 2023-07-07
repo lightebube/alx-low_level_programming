faec7ca1866453fff434ba8532a07cf80f45783b
lightebube <lightebube751@gmail.com>
2023-07-06 19:45:22 -0700

    A fuction that checks if a string is a palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,15 +13,12 @@ int is_palindrome(char *s)
 	int start = 0;
 	int end = len - 1;
 
-	while (start < end)
-	{
-		if (s[start] != s[end])
+	if (s[start] != s[end])
 		{
 			return (0);
 		}
 		start++;
 		end--;
-	}
 
 	return (1);
 }
