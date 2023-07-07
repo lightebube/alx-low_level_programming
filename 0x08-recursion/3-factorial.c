0b4bf1cd7a43ccc8b85e00886f7d7e389e4aab3d
lightebube <lightebube751@gmail.com>
2023-07-06 18:48:13 -0700

    A function that finds the factorial of a number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,17 +10,17 @@ int factorial(int n)
 {
 	if (n < 0)
 	{
-		return(-1);
+		return (-1);
 	}
 	if (n == 0 || n == 1)
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(n * factorial(n - 1));
+		return (n * factorial(n - 1));
 		factorial(n);
 	}
 
-	return(0);
+	return (0);
 }
