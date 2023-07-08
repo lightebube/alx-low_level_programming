28ecae09da0577653106e101ee1dc7860b785c3b
lightebube <lightebube751@gmail.com>
2023-07-07 17:52:46 -0700

    Argument in main

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiples two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 1
+ */
+int main(int argc, char *argv[])
+{
+	int l = 1;
+	int i;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			l *= atoi(argv[i]);
+		}
+		printf("%d\n", l);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+
+	return (1);
+}
