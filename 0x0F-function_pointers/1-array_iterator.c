ed1d32cde8b5648c0cb0ed437827088595175a04
lightebube <lightebube751@gmail.com>
2023-07-19 18:55:16 -0700

    Function_pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * array_iterator - assigns a number of arrays
+ * @array: array name
+ * @size: size of the array
+ * @action: fuction pointer
+ *
+ * Return: void, nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i;
+
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
+#include <stddef.h>
 #ifndef MAIN_H
 #define MAIN_H
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void(*action)(int));
 
 
 #endif /*MAIN_H*/
