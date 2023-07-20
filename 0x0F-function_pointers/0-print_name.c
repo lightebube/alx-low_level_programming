59338b412aedfdb034270d310af5af2c05386298
lightebube <lightebube751@gmail.com>
2023-07-19 18:33:44 -0700

    Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_name - prints name
+ * @name: name
+ * @f: function pointer
+ *
+ * Return: void, nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+
+	f(name);
+}
